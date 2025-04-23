a2136143b3eb2f5e0928a4ed2de4644bd45e4a95
Ty700 <T.ScottiBusiness@yahoo.com>
2025-04-22 20:14:55 -0500

    Add remaps, and config nvim interface.

--- lua/ty700/init.lua
+++ lua/ty700/init.lua
@@ -1,1 +1,23 @@
 require("ty700.remap")
+require("ty700.set")
+
+local augroup = vim.api.nvim_create_augroup
+local Ty700 = augroup('Ty700', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
