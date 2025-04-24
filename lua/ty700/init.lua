313ea6df016af8e0a0035647b45138e9ee60b932
Ty700 <T.ScottiBusiness@yahoo.com>
2025-04-23 20:56:30 -0500

    Add LSPs

--- lua/ty700/init.lua
+++ lua/ty700/init.lua
@@ -1,11 +1,33 @@
 require("ty700.remap")
 require("ty700.set")
 
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"clangd",
+		"html",
+		"cssls",
+		"ts_ls",
+		"jdtls",
+		"pyright",
+		"lua_ls"
+	},
+})
+
+require'cmp'.setup {
+	sources = {
+		{ name = 'nvim_lsp' }
+	}
+}
+
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 local augroup = vim.api.nvim_create_augroup
 local Ty700 = augroup('Ty700', {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup('HighlightYank',{})
 
 autocmd('TextYankPost', {
     group = yank_group,
@@ -21,3 +43,41 @@ autocmd('TextYankPost', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+lspconfig.clangd.setup({
+	cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+	capabilities = capabilities,
+	init_options = {
+		fallbackFlags = { '-std=c++17' },
+	},
+})
+
+-- HTML
+lspconfig.html.setup({
+    capabilities = capabilities,
+})
+
+-- CSS
+lspconfig.cssls.setup({
+    capabilities = capabilities,
+})
+
+-- JavaScript/TypeScript
+lspconfig.ts_ls.setup({
+    capabilities = capabilities,
+})
+
+-- Java
+lspconfig.jdtls.setup({
+    capabilities = capabilities,
+})
+
+-- Python
+lspconfig.pyright.setup({
+    capabilities = capabilities,
+})
+
+-- Lua
+lspconfig.lua_ls.setup({
+    capabilities = capabilities,
+})
--- lua/ty700/packer.lua
+++ lua/ty700/packer.lua
@@ -4,34 +4,48 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	-- LSP
+	use 'neovim/nvim-lspconfig'
+	
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  use ({
+	use ({
 	  'rose-pine/neovim',
 	  as = 'rose-pine',
 	  config = function()
 		  vim.cmd('colorscheme rose-pine')
 	  end
-  })
+	})
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+ 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+  	use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
 
-  use "mbbill/undotree" 
-  use "tpope/vim-fugitive" 
+  	use "mbbill/undotree" 
+  
+	use "tpope/vim-fugitive" 
  
-  use {
+  	use {
 	  "ThePrimeagen/harpoon",
 	  branch = "harpoon2",
 	  requires = { {"nvim-lua/plenary.nvim"} }
-  }
+ 	}
 
+	use( 'neovim/nvim-lspconfig')	
+	use( 'hrsh7th/cmp-nvim-lsp')
+	use( 'hrsh7th/cmp-buffer')
+	use( 'hrsh7th/cmp-path')
+	use( 'hrsh7th/cmp-cmdline')
+	use( 'hrsh7th/nvim-cmp')
+	use {
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
+	}
 end)
