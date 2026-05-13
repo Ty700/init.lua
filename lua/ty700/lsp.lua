2ee625a2c758d498b79848c3c50d913a32e2b72a
Tyler Scotti <T.ScottiBusiness@yahoo.com>
2026-05-12 23:25:09 -0500

    Move to new lsp install

--- lua/ty700/lsp.lua
+++ lua/ty700/lsp.lua
@@ -1,63 +1,33 @@
 -- Setup Mason
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "clangd",
-        "html",
-        "cssls",
-        "ts_ls",
-        "jdtls",
-        "pyright",
-        "lua_ls"
-    },
+  ensure_installed = {
+    "clangd", "html", "cssls", "ts_ls",
+    "jdtls", "pyright", "lua_ls"
+  },
 })
 
--- Configure LSP
-local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- Set defaults that apply to all servers
+vim.lsp.config('*', { capabilities = capabilities })
+
 -- C/C++
-lspconfig.clangd.setup({
-    cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose' },
-    capabilities = capabilities,
-    init_options = {
-        fallbackFlags = { '-std=c++17' },
-    },
+vim.lsp.config('clangd', {
+  cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose' },
+  init_options = { fallbackFlags = { '-std=c++17' } },
 })
 
--- HTML
-lspconfig.html.setup({
-    capabilities = capabilities,
+-- HTML / CSS / TS / Java / Python / Lua: no extra config needed
+
+-- Swift
+vim.lsp.config('sourcekit', {
+  cmd = { 'xcrun', 'sourcekit-lsp' },
+  filetypes = { 'swift', 'objective-c', 'objective-cpp' },
 })
 
--- CSS
-lspconfig.cssls.setup({
-    capabilities = capabilities,
-})
-
--- JavaScript/TypeScript
-lspconfig.ts_ls.setup({
-    capabilities = capabilities,
-})
-
--- Java
-lspconfig.jdtls.setup({
-    capabilities = capabilities,
-})
-
--- Python
-lspconfig.pyright.setup({
-    capabilities = capabilities,
-})
-
--- Lua
-lspconfig.lua_ls.setup({
-    capabilities = capabilities,
-})
-
--- Swift 
-lspconfig.sourcekit.setup({
-    capabilities = capabilities,
-    cmd = { 'xcrun', 'sourcekit-lsp' },
-    filetypes = { 'swift', 'objective-c', 'objective-cpp' },
+-- Enable them all
+vim.lsp.enable({
+  'clangd', 'html', 'cssls', 'ts_ls',
+  'jdtls', 'pyright', 'lua_ls', 'sourcekit'
 })
